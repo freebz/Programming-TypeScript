010d1dc08f95c9766d51bc923c4b700a36c022b4
freebz <freebz.dev@gmail.com>
2023-03-17 23:16:41 +0900

    2장 타입스크립트: 3,000미터 상공에서 내려다보기

---
+++ chapter-2/ex2-1.ts
@@ -0,0 +1,11 @@
+// 2.2 타입 시스템
+
+let a: number = 1                 // a는 number
+let b: string = 'hello'           // b는 string
+let c: boolean[] = [true, false]  // c는 boolean 배열
+
+
+
+let a = 1              // a는 number
+let b = 'hello'        // b는 string
+let c = [true, false]  // c는 boolean 배열
